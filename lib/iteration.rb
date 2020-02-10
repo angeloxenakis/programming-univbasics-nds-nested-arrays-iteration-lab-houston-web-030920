3a0aa26838c9dbabd74b08969894618f2cc4aca9
Angelo Xenakis <angeloxenakis@gmail.com>
2020-02-10 03:22:47 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -24,8 +24,8 @@ def find_greater_pair(src)
   greater_nums = []
   while row > src.length do 
     if src[row][0] > src[row][1]
-      greater_nums.push(src[row][0])
-    else greater_nums.push(src[row][1])
+      greater_nums << (src[row][0])
+    else greater_nums << (src[row][1])
     end
   row += 1 
   end
