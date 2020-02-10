39d35b2a5922fb4c0e9f0a22612a82c5c9b54133
Angelo Xenakis <angeloxenakis@gmail.com>
2020-02-10 03:36:30 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -33,7 +33,7 @@ def find_greater_pair(src)
     row_index += 1
   end
     return result
-end	end
+end	
 
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # if both numbers in the pair are even, then add both those numbers to the
